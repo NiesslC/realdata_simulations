5ca47b4d605eef48e40c228fcd6fa1691b9288ba
Christina Niessl <christina.niessl@gmx.de>
2023-11-16 16:55:32 +0100

    added generation of datasets with meta inforamtion on tcga parameters (dispersion and mean)

--- deanalysis/code/02_script_get_tcga_parameters.R
+++ deanalysis/code/02_script_get_tcga_parameters.R
@@ -1,5 +1,8 @@
 library(dplyr)
 library(edgeR)
+library(reshape2)
+library(stringr)
+library(purrr)
 source("./deanalysis/code/_fcts.R")
 
 
@@ -24,7 +27,6 @@ tcga_datasets_paired = tcga_datasets_paired[!is.na(tcga_datasets_paired)]
 
 # Get parameters -----------------------------------------------------------------------------------
 
-
 tcga_parameters =  vector("list", length = length(tcga_datasets_paired))
 names(tcga_parameters) = names(tcga_datasets_paired)
 for(i in 1:length(tcga_datasets_paired)){
@@ -34,3 +36,61 @@ rm(i)
 save(tcga_parameters, file = "./deanalysis/data/tcga_parameters.RData")
 
 
+# Get parameter meta data --------------------------------------------------------------------------
+names(tcga_parameters) = word(names(tcga_parameters) ,1,sep = "\\_")
+
+## Get information on number of samples and genes ----------------------------------------------------
+nsample = purrr::map_depth(tcga_parameters, 1, "k_count") %>% purrr::map(. ,~ ncol(.)/2)
+ngene = purrr::map_depth(tcga_parameters, 1, "k_count") %>% purrr::map(. ,~ nrow(.))
+nfilter = purrr::map_depth(tcga_parameters, 1, "k_index.filter") %>% purrr::map(. ,~ length(.))
+data_info = data.frame(nsample = as_vector(nsample), ngene = as_vector(ngene),
+                       nfilter = as_vector(nfilter))
+dataset_names = names(nsample)
+data_info = data_info %>% mutate(dataset = dataset_names,
+                                 ngenes_final = ngene-nfilter)
+rm(nsample, ngene, nfilter, dataset_names)
+
+## Extract parameters for mean, dispersion ----------------------------------------------------------
+mean.normal_all = purrr::map_depth(tcga_parameters, 1, "mean.normal") 
+mean.normal_all = reshape2::melt(mean.normal_all, value.name = "mean") %>% dplyr::rename(dataset = L1)
+mean.cancer_all = purrr::map_depth(tcga_parameters, 1, "mean.cancer") 
+mean.cancer_all = melt(mean.cancer_all, value.name = "mean") %>% dplyr::rename(dataset = L1)
+mean.total_all = purrr::map_depth(tcga_parameters, 1, "k_mean.total") 
+mean.total_all = melt(mean.total_all, value.name = "mean") %>% dplyr::rename(dataset = L1)
+
+disp.normal_all = purrr::map_depth(tcga_parameters, 1, "disp.normal") 
+disp.normal_all = melt(disp.normal_all, value.name = "disp") %>% dplyr::rename(dataset = L1)
+disp.cancer_all = purrr::map_depth(tcga_parameters, 1, "disp.cancer") 
+disp.cancer_all = melt(disp.cancer_all, value.name = "disp") %>% dplyr::rename(dataset = L1)
+disp.total_all = purrr::map_depth(tcga_parameters, 1, "k_disp.total") 
+disp.total_all = melt(disp.total_all, value.name = "disp") %>% dplyr::rename(dataset = L1)
+
+
+mean.normal_all = full_join(mean.normal_all, data_info, by = "dataset")%>% 
+  mutate(dataset = factor(dataset, levels = c(data_info$dataset[order(data_info$nsample)])))
+mean.cancer_all = full_join(mean.cancer_all, data_info, by = "dataset")%>% 
+  mutate(dataset = factor(dataset, levels = c(data_info$dataset[order(data_info$nsample)])))
+mean.total_all = full_join(mean.total_all, data_info, by = "dataset")%>% 
+  mutate(dataset = factor(dataset, levels = c(data_info$dataset[order(data_info$nsample)])))
+disp.normal_all = full_join(disp.normal_all, data_info, by = "dataset")%>% 
+  mutate(dataset = factor(dataset, levels = c(data_info$dataset[order(data_info$nsample)])))
+disp.cancer_all = full_join(disp.cancer_all, data_info, by = "dataset")%>% 
+  mutate(dataset = factor(dataset, levels = c(data_info$dataset[order(data_info$nsample)])))
+disp.total_all = full_join(disp.total_all, data_info, by = "dataset")%>% 
+  mutate(dataset = factor(dataset, levels = c(data_info$dataset[order(data_info$nsample)])))
+
+save(mean.normal_all,mean.cancer_all,mean.total_all,disp.normal_all,disp.cancer_all,disp.total_all, 
+     file = "./deanalysis/data/tcga_parameters_metadata.RData")
+
+# Get parameters from compareDEtools (for comparison) ----------------------------------------------
+library(compareDEtools)
+param = generateDatasetParameter()
+# only keep kirc parameters
+param=param[c("k_count","disp.normal","mean.normal","disp.cancer","mean.cancer",
+      "k_mean.total","k_index.filter","k_disp.total")]
+# compare "k_count","k_mean.total","k_index.filter","k_disp.total"
+cbind(dim(param$k_count),dim(tcga_parameters$`KIRC_RNASeq2Gene-20160128`$k_count))
+cbind(length(param$k_index.filter),length(tcga_parameters$`KIRC_RNASeq2Gene-20160128`$k_index.filter))
+cbind(summary(param$k_mean.total),summary(tcga_parameters$`KIRC_RNASeq2Gene-20160128`$k_mean.total))
+cbind(summary(param$k_disp.total),summary(tcga_parameters$`KIRC_RNASeq2Gene-20160128`$k_disp.total))
+# -> very similar
