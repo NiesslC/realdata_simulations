ca16858dd705464b1f7e74b1c3b10649dda0f88c
Christina Niessl <christina.niessl@gmx.de>
2023-11-16 16:55:00 +0100

    added code for generating performance dataset for no de genes case

--- deanalysis/code/04_script_performdat.R
+++ deanalysis/code/04_script_performdat.R
@@ -26,12 +26,17 @@ param.fig2$modes = c('D','R','OS')
 param.fig2$rowType = c('AUC','TPR','trueFDR')
 param.fig2$fixedfold = FALSE
 
-dataset.dir='./deanalysis/data/'  #
 analysis.dir='./deanalysis/results/rdata/' 
-figure.dir='./deanalysis/results/plots/'
 
 ## Fig3 in Baik ----
-# [to do]
+param.fig3 = list()
+param.fig3$nvar = 10000
+param.fig3$rep.end = 50 
+param.fig3$AnalysisMethods =c('edgeR','edgeR.ql','edgeR.rb','DESeq.pc','DESeq2','voom.tmm','voom.qn','voom.sw','ROTS','BaySeq','PoissonSeq','SAMseq')
+param.fig3$nsample = c(3,10)
+param.fig3$disp.Types = 'same'
+param.fig3$modes = c('D','R','OS')
+
 
 # Generate performance measures datasets -----------------------------------------------------------
 ## Fig. 2 in Baik (settings with > 0 DE genes) ----
@@ -40,7 +45,6 @@ for(i in 1:length(data.types.names)){
   performdat_list[[i]] = vector("list", length = length(param.fig2$modes))
   for(j in 1:length(param.fig2$modes)){
    performdat_list[[i]][[j]] = performance_plot_new(working.dir=paste0(getwd(),"/deanalysis/results/rdata/rdata_degenes/"),
-                                      figure.dir=figure.dir,
                                       fixedfold=param.fig2$fixedfold,
                                       simul.data=data.types.names[i],
                                       rep.start=1,
@@ -49,7 +53,7 @@ for(i in 1:length(data.types.names)){
                                       nvar=param.fig2$nvar, 
                                       nDE=param.fig2$nDE, 
                                       fraction.upregulated = param.fig2$fraction.upregulated,
-                                      disp.Types = param.fig2$disp.Types,
+                                      disp.Type = param.fig2$disp.Types,
                                       mode=param.fig2$modes[j],
                                       AnalysisMethods=param.fig2$AnalysisMethods[-which(param.fig2$AnalysisMethods == "SAMseq")],###!
                                       rowType = param.fig2$rowType)
@@ -57,22 +61,45 @@ for(i in 1:length(data.types.names)){
 }
 
 
-performdat = bind_rows(performdat_list)
-save(performdat, file = "./deanalysis/results/rdata/performdat_degenes.RData")
-
+performdat_degenes = bind_rows(performdat_list)
+save(performdat_degenes, file = "./deanalysis/results/rdata/performdat_degenes.RData")
+rm(performdat_list)
 ###
 # Note:
 # - For trueFDR, there are settings/datasets with NAs. This is because "we calculated true FDR only
 #   when five or more significant genes were detected in each method" (Baik et al.), i.e.,
 #   when length(which(FDR < 0.1)) <= 5 (see function performance_plot_new)
-table(is.na(performdat$trueFDR))
-table(is.na(performdat$TPR))
-table(is.na(performdat$AUC))
-# - Currently no performance measures for SAMseq are calculated
+table(is.na(performdat_degenes$trueFDR))
+table(is.na(performdat_degenes$TPR))
+table(is.na(performdat_degenes$AUC))
+# - Currently no performance measures for SAMseq are calculated (also below)
 ###
 
 
 ## Fig. 3 in Baik (settings with = 0 DE genes) ----
+performdat_list =  vector("list", length = length(data.types.names))
+for(i in 1:length(data.types.names)){
+  performdat_list[[i]] = vector("list", length = length(param.fig3$modes))
+  for(j in 1:length(param.fig3$modes)){
+    performdat_list[[i]][[j]] = fpc_performance_plot_new(working.dir=paste0(getwd(),"/deanalysis/results/rdata/rdata_nodegenes/"),
+                                                         simul.data=data.types.names[i],
+                                                         rep.start=1,
+                                                         rep.end=param.fig3$rep.end, 
+                                                         nsample=param.fig3$nsample , 
+                                                         disp.Type =param.fig3$disp.Types, 
+                                                         mode=param.fig3$modes[j],
+                                                         AnalysisMethods=param.fig3$AnalysisMethods[-which(param.fig3$AnalysisMethods == "SAMseq")],###!
+                                                         nvar = param.fig3$nvar)
+  }
+}
 
-# [to do]
+
+performdat_nodegenes = bind_rows(performdat_list)
+save(performdat_nodegenes, file = "./deanalysis/results/rdata/performdat_nodegenes.RData")
+rm(performdat_list)
+
+
+
+
+
 
